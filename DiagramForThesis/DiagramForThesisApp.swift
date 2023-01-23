1caf913cedbc07d8c21819aff29aa5c6d59d4ae4
OnagaHaruki <haloupgarou@gmail.com>
2023-01-24 06:49:11 +0900

    Initial Commit

---
+++ DiagramForThesis/DiagramForThesisApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiagramForThesisApp.swift
+//  DiagramForThesis
+//
+//  Created by オナガ・ハルキ on 2023/01/24.
+//
+
+import SwiftUI
+
+@main
+struct DiagramForThesisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
